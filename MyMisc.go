2a744cafa56f2d5c7b0fcbb0067770633bcaa777
zl <netldds@tianhong.com>
2018-05-25 18:09:07 +0800

    branch development update

--- MyMisc.go
+++ MyMisc.go
@@ -32,8 +32,13 @@ func (s *t_face_struct) Check() bool {
 	return true
 }
 
+//返回接口类型
 func test_t_interface(t t_face) t_face {
 
 	return &t_face_struct{}
 
+}
+
+func assert(i interface{}) {
+	s:=i.(int)
 }
