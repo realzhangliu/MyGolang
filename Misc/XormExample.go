639c96337ef79a751b9da9d69f54c33cc3a910be
zhangliu <netldds@163.com>
2018-08-17 00:42:04 +0800

    1

--- Misc/XormExample.go
+++ Misc/XormExample.go
@@ -5,7 +5,6 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
-	"time"
 )
 
 var engine *xorm.Engine
@@ -17,24 +16,36 @@ type labor struct {
 	Occupation string `xorm:"varchar(30) notnull"`
 }
 
+var DBSource string = "ml:0000@tcp(192.168.1.109)/person?charset=utf8mb4"
+var DBSource_loal string = "root:0000@/person?charset=utf8mb4"
+
 func XormExample() {
 	var err error
-	engine, err = xorm.NewEngine("mysql", "ml:0000@/person?charset=utf8mb4")
+	engine, err = xorm.NewEngine("mysql", DBSource)
 	checkError(err)
 	err = engine.Ping()
 	var lb labor
-	_, err = engine.Table("labor_bak").Where("ID=?", "2").Get(&lb)
-	//fmt.Println(lb, flag)
+	_, err = engine.Table("labor").Where("ID=?", "1").Get(&lb)
+	fmt.Println(lb)
 
 	var pwd []string
-	engine.Table("labor_bak").Cols("password").Find(&pwd)
-	//fmt.Println(pwd)
+	engine.Table("labor").Cols("name").Find(&pwd)
+	fmt.Println(pwd)
 
-	cols := []string{"name", "age", "password"}
+	cols := []string{"name", "age"}
 	var valuesSlice = make([]interface{}, len(cols))
-	engine.Table("labor_bak").Cols(cols...).Get(&valuesSlice)
+	engine.Table("labor").Cols(cols...).Get(&valuesSlice)
 	fmt.Println(string(valuesSlice[1].([]byte)))
-time.Now().Format()
+
+	lb = labor{
+		Name: "zl",
+	}
+	ok, err := engine.Table("labor").Exist(&lb)
+	if ok {
+		fmt.Println("FOUND.")
+	}
+
+
 }
 
 func backup() {
